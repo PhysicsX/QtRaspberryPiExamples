8754739f6e0edaa3a9033f55e052542938a31de9
PhysicsX <ulasdikme@gmail.com>
2020-05-01 20:09:25 +0300

    Ltc2400 code example for reading analog value

--- speedometer/main.cpp
+++ speedometer/main.cpp
@@ -2,6 +2,10 @@
 #include <QQmlApplicationEngine>
 #include <QTimer>
 #include "speedometer.h"
+#include <wiringPiSPI.h>
+#include <unistd.h>
+
+static const int CHANNEL = 1;
 
 int main(int argc, char *argv[])
 {
@@ -23,12 +27,18 @@ int main(int argc, char *argv[])
     qreal val = 110;
     ptrSpeedometer->setSpeed(val);
 
-    bool direction = true;
+    //bool direction = true;
     QTimer timer;
+
+
+    float volt = 0;
 
     QObject::connect(&timer, &QTimer::timeout, [&]()
     {
         ptrSpeedometer->setSpeed(val);
+
+       // val = volt;
+
         if(val < 1600)
                 ptrSpeedometer->setOuterColor(QColor(128,255,0));
         else if(val > 1600 && val < 3200)
@@ -36,19 +46,64 @@ int main(int argc, char *argv[])
         else if(val > 3200)
                 ptrSpeedometer->setOuterColor(QColor(255,0,0));
 
-        if(val >= 4000)
-            direction = false;
-        else if(val <= 0.1)
-            direction = true;
+        if(abs(volt-val) <=100)
+            val = volt;
+        else if(volt > val)
+            val = val + 100;
+        else if( volt < val)
+            val = val - 100;
 
-        if(direction)
-            val = val + 10;
-        else
-            val = val - 10;
+
+//        if(val >= 4000)
+//            direction = false;
+//        else if(val <= 0.1)
+//            direction = true;
+
+//        if(direction)
+//            val = val + 10;
+//        else
+//            val = val - 10;
 
     }
     );
 
+
+    unsigned char buffer[100];
+    wiringPiSPISetup(CHANNEL, 1000000);
+
+    QTimer timer2;
+     long int ltw = 0;
+    QObject::connect(&timer2,&QTimer::timeout, [&]()
+    {
+
+        wiringPiSPIDataRW(CHANNEL, buffer, 4);
+
+       // bool sig;
+        ltw = 0;
+
+        //f((buffer[0]&0x20)==0) sig = 1;
+                buffer[0] &= 0x1F;
+                ltw |= buffer[0];
+                ltw <<= 8;
+                ltw |= buffer[1];
+                ltw <<= 8;
+                ltw |= buffer[2];
+                ltw <<= 8;
+                ltw |= buffer[3];
+
+                //if(sig) ltw |= 0xf0000000;
+                ltw = ltw/16;
+                volt = ltw*4000.1 / 16777216;
+             //   qDebug()<<(unsigned int)volt;
+
+    });
+
+   // usleep(180000);
+
+
+
+
+    timer2.start(180);
     timer.start(10);
 
     if (engine.rootObjects().isEmpty())
